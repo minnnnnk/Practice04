6d3102d933e4ddb3148c0393b246f038b67f4b48
mingyu <jubal9153@gmail.com>
2022-04-20 16:55:49 +0900

    과제

---
+++ src/com/javaex/practice/Ex02.java
@@ -0,0 +1,19 @@
+package com.javaex.practice;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		
+		double[] bArray = new double[3] ; //생성
+		
+		bArray[0] = 6.7; // 값
+		bArray[1] = 3.3;
+		bArray[2] = 1.2;
+		//출력
+		for (int i = 0; i<bArray.length; i++) {
+			System.out.println(bArray[i]);
+		}
+		
+	}
+
+}
