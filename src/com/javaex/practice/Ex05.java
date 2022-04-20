9d7d720ec449473087a36842cf1ee917c9ffb2fc
mingyu <jubal9153@gmail.com>
2022-04-20 17:03:32 +0900

    2022-04-20 Ex05

--- src/com/javaex/practice/Ex05.java
+++ src/com/javaex/practice/Ex05.java
@@ -1,10 +1,26 @@
 package com.javaex.practice;
 
+import java.util.Scanner;
+
 public class Ex05 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		double[] nums = new double[5];
+		double sum = 0;
+		
+		for (int i = 0; i<nums.length; i++) {
+			nums [i] = sc.nextInt();
+			sum = sum+nums[i];
+			
+		}
+		System.out.println("평균은 " +(sum/nums.length)+ " 입니다.");
+		
+		
+		
+		
+		sc.close();
 	}
 
 }
